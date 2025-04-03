ba37d77240925c3fb2364255e8bd2cd67b711648
MayconProg <maycondouglass.mg@gmail.com>
2025-04-03 17:23:52 -0300

    Initial Commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Server Configs
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+const port = Number(process.env.PORT) || 2108;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`HTTP Server Running on http://localhost:${port} ☕`);
+});
